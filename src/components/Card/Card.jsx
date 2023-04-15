c28fc87539ae79b76a5fd2814332eadf48bb43ef
najot <otabekyusupov3738@gmail.com>
2023-04-16 02:04:47 +0500

    adds

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import "./index.scss";
 import wapka from '../../assets/images/wapka.svg';
 import wapka2 from '../../assets/images/2.svg';
 import wapka3 from '../../assets/images/3.svg';
 import wapka4 from '../../assets/images/4.svg';
+import {context} from '../context/context'
 const Card = () => {
+   const { mode,LANG,lang } = useContext(context);
+   const t=LANG[lang.toLowerCase()];
 
     // const {item:{email, first_name}} = props;
     return (
@@ -20,22 +23,22 @@ const Card = () => {
                  <div className='d-flex gap-5 card-box'>
                   <div className='card1'>
                     <img src={wapka } alt=""  width={48} height={38}/>
-                    <p>Students</p>
+                    <p>{t.students}</p>
                     <p className='card-num'>243</p>
                  </div>
                  <div className='card2'>
                     <img src={wapka2 } alt=""  width={48} height={38}/>
-                    <p>Course</p>
+                    <p>{t.course}</p>
                     <p className='card-num'>13</p>
                  </div>
                  <div className='card3'>
                     <img src={wapka3 } alt=""  width={48} height={38}/>
-                    <p>Payments</p>
+                    <p>{t.payments}</p>
                     <p className='card-num'>556,000</p>
                  </div>
                  <div className='card4'>
                     <img src={wapka4 } alt=""  width={48} height={38}/>
-                    <p>Users</p>
+                    <p>{t.users}</p>
                     <p className='card-num'>3</p>
                  </div>
                  </div>
